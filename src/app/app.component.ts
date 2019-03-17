d7304c84ce2433146825884ae6a920749549d078
enzo <a08160803@gmail.com>
2019-03-18 07:43:58 +0800

    41:24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TestComponent';
+  name = 'Angular';
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  showSpan = false;
+
+  show() {
+    alert('123456789');
+  }
+
+  showValue(value: HTMLElement) {
+    console.log(value);
+    value.innerHTML = 'change something else';
+  }
+
+  doSomethingWhenModelChange(value) {
+    console.log(value);
+    this.name = value;
+  }
+
+  getEvent(event) {
+    console.log(event);
+    this.name;
+  }
 }
